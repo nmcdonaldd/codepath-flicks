8bc8b32f9d5021bfb64f0ab10326f9e57b0916ad
Nicholas McDonald <nmmcdona@ucsd.edu>
2017-01-10 09:37:18 -0800

    More UI updates: moved movie information to bottom of cell. Adjusted the gradient layer on the backdrop image.

--- flicks/MoviesTableViewCell.swift
+++ flicks/MoviesTableViewCell.swift
@@ -19,11 +19,11 @@ class MoviesTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         
-        //let view = UIView(frame: CGRect(x: self.contentView.frame.origin.x, y: self.contentView.frame.origin.y, width: self.contentView.frame.width, height: self.contentView.frame.height))
         let gradient = CAGradientLayer()
         gradient.frame = self.moviePosterImageView.bounds
-        gradient.colors = [UIColor.black.cgColor, UIColor.clear.cgColor]
-        gradient.locations = [0.0, 1.0]
+        let blackColor: CGColor = UIColor(colorLiteralRed: 0.0, green: 0.0, blue: 0.0, alpha: 0.70).cgColor
+        gradient.colors = [UIColor.clear.cgColor, blackColor]
+        gradient.locations = [0.5, 1.0]
         self.moviePosterImageView.layer.insertSublayer(gradient, at: 0)
     }
 
